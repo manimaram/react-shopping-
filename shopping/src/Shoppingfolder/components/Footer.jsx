d2ed5c7ed1f7b22228a009d2f362eaa60a6944bc
MARAM NAGAMANI <manimaram781@gmail.com>
2025-03-17 23:31:01 +0530

    Update Footer.jsx

--- shopping/src/Shoppingfolder/components/Footer.jsx
+++ shopping/src/Shoppingfolder/components/Footer.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footersection'>
+      <div className="bannerbox">
+            <img src="Assests/footer.png" alt="banner"/>
+        </div>
       </div>
   )
 }
